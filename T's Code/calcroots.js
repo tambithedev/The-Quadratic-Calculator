7a58d51b6f0e7ced6985daa42a1670d64489af5d
tambithedev <tambiraishe.magadzire@gmail.com>
2024-11-28 17:54:50 +0200

    Update calcroots.js

--- T's Code/calcroots.js
+++ T's Code/calcroots.js
@@ -1,4 +1,9 @@
 function calcroots(a, b, c) {
+	if ((isNaN(a)) || (is NaN(b)) || (isNaN(c))) {
+		window.alert("Invalid Input.")
+		return "";
+	}
+
 	let D;
 	D = (Math.pow(b, 2)) - (4 * a * c);
 	let root1;
