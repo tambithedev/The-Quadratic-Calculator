67a5e9c7b3ab5017098e77da97dc5366f0d087f2
tambithedev <tambiraishe.magadzire@gmail.com>
2024-11-28 18:01:44 +0200

    Update selectwao.js

--- T's Code/selectwao.js
+++ T's Code/selectwao.js
@@ -1,7 +1,7 @@
 function selectWao() {
 //each different page/joke has a number assigned to it
 	let randomSelection;
-	randomSelection = Math.floor(Math.random() * 10) + 1; //random number between 1 and 10 (inclusive)
+	randomSelection = Math.floor(Math.random() * 9) + 1; //random number between 1 and 9 (inclusive)
 	switch(randomSelection) {
 	//cases 1-4: go to the "regular format" wrong answer page, exiting with a parameter
 		case 1:
@@ -26,12 +26,9 @@ function selectWao() {
 			location.href = /*"sleepPageUrl"*/;
 			break;
 		case 8:
-			location.href = /*"sleepPageUrl"*/;
-			break;
-		case 9:
 			location.href = /*"graphPageUrl"*/;
 			break;
-		case 10:
+		case 9:
 			location.href = /*"-1/12PageUrl"*/;
 	}
 }
