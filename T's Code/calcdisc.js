5a13e5c1517e20bc3200387fac541225985cfc1d
tambithedev <tambiraishe.magadzire@gmail.com>
2024-11-28 17:55:56 +0200

    Update calcdisc.js

--- T's Code/calcdisc.js
+++ T's Code/calcdisc.js
@@ -1,4 +1,8 @@
 function calcdisc(a, b, c) {
+		if ((isNaN(a)) || (is NaN(b)) || (isNaN(c))) {
+		window.alert("Invalid Input.")
+		return "";
+	}
 	let D;
 	D = (Math.pow(b, 2)) - (4 * a * c);
 	return D;
