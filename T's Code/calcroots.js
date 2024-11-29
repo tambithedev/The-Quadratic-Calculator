222065f61491f0231458453a70a47b453173023e
tambithedev <tambiraishe.magadzire@gmail.com>
2024-11-29 09:00:24 +0200

    Update calcroots.js

--- T's Code/calcroots.js
+++ T's Code/calcroots.js
@@ -1,6 +1,6 @@
-function calcroots(a, b, c) {
-	if ((isNaN(a)) || (is NaN(b)) || (isNaN(c))) {
-		window.alert("Invalid Input.")
+function calcRoots(a, b, c) {
+	if ((isNaN(a)) || (isNaN(b)) || (isNaN(c))) {
+		window.alert("Invalid Input.");
 		return "";
 	}
 
