43afffd0731f5dc47d055a2be0ccea7d4509bd8a
tambithedev <tambiraishe.magadzire@gmail.com>
2024-12-01 03:50:39 +0200

    Update showsteps.js
    see previous change.

--- T's Code/showsteps.js
+++ T's Code/showsteps.js
@@ -5,10 +5,6 @@ function showSteps(a, b, c) {
 		window.alert("Invalid Input.");
 		return "";
 	}
-
-	//show variables being used at the beginning
-	document.getElementById("displayCoefficients").innerHTML =
-		"a = " + a + "<br>" + "b = "+ b + "<br>" + "c = " + c;
 	
 	//define variables to be used in next step(s) for simplification, and get the solution
 	let E;
